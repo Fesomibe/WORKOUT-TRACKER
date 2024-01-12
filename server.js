98080c9b4610c2d9c1d720f6b6898ed9614a6a70
Fesomibe <francisesomibe1@gmail.com>
2024-01-12 14:22:58 -0600

    Initial commit

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var methodOverride = require('method-override');
+
 require('dotenv').config();
 require('./config/database');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/');
@@ -20,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
 app.use('/app', usersRouter);
@@ -33,7 +37,7 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.server.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
